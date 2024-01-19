e7a1cf247fb212ef2e99e073e0f1dd33f20fca53
natalie <hardman.natalie85@gmail.com>
2024-01-19 14:55:13 +0000

    change local url to render url

--- client/config.js
+++ client/config.js
@@ -1,3 +1,3 @@
-export const API_URL="http://localhost:8080/posts";
+// export const API_URL="http://localhost:8080/posts";
 
-// export const API_URL=""
+export const API_URL="https://week7-project.onrender.com/posts"
